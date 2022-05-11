9e99e742f4193494c9cf284f45f31934581589f4
FabioR <faitel92@hotmail.it>
2022-05-11 13:22:46 +0200

    fix

--- Index/FindingUnusedIndexes.sql
+++ Index/FindingUnusedIndexes.sql
@@ -9,7 +9,6 @@ FROM
     INNER JOIN sys.objects ON dm_db_index_usage_stats.OBJECT_ID = objects.OBJECT_ID
     INNER JOIN sys.indexes ON indexes.index_id = dm_db_index_usage_stats.index_id AND dm_db_index_usage_stats.OBJECT_ID = indexes.OBJECT_ID
 WHERE
-    AND
     dm_db_index_usage_stats.user_lookups = 0
     AND
     dm_db_index_usage_stats.user_seeks = 0
